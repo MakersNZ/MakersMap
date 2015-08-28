db0025d067ad5d927a319357b1820528e6f2afad
Joran Kikke <joran.k@gmail.com>
2015-08-29 11:10:56 +1200

    spec fixes for two user models

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -2,8 +2,6 @@ FactoryGirl.define do
   factory :person do
     sequence(:email){|n| "person_#{n}@testing.com" }
     
-    password "123123999"
-    password_confirmation { "123123999" }
   end
 
 end
