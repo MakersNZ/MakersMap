6fe19b4fe012a5d46e7f654abbd7df6ab7e25b93
Joran Kikke <joran.k@gmail.com>
2015-12-02 18:42:10 +1300

    markers for all hits

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,10 @@ class SearchController < ApplicationController
   def index
     if params[:q].blank?
       @results = Elasticsearch::Model.search( { query: { match_all: {} } }, [Place, Person]).page(params[:page]).records
+      @all_results = Elasticsearch::Model.search( { query: { match_all: {} } }, [Place, Person]).records
     else
       @results = Elasticsearch::Model.search(params[:q], [Place]).page(params[:page]).records
+      @all_results = Elasticsearch::Model.search(params[:q], [Place]).records
     end
     @tags = Tag.all
   end
