7fb8c18cd9142a3844328fb02cba64d60545bffb
Joran Kikke <joran.k@gmail.com>
2015-12-03 21:07:14 +1300

    larger result pool for map pins + map layout fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,14 @@
 class SearchController < ApplicationController
 
+  RESULT_SET = 300
+
   def index
     if params[:q].blank?
       @results = Elasticsearch::Model.search( { query: { match_all: {} } }, [Place, Person]).page(params[:page]).records
-      @all_results = Elasticsearch::Model.search( { query: { match_all: {} } }, [Place, Person]).records
+      @all_results = Elasticsearch::Model.search( { query: { match_all: {} }, size: RESULT_SET }, [Place, Person]).records.to_a
     else
-      @results = Elasticsearch::Model.search(params[:q], [Place]).page(params[:page]).records
-      @all_results = Elasticsearch::Model.search(params[:q], [Place]).records
+      @results = Elasticsearch::Model.search(params[:q], [Place, Person]).page(params[:page]).records
+      @all_results = Elasticsearch::Model.search(params[:q], [Place, Person]).records.to_a
     end
     @tags = Tag.all
   end
