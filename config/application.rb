03ce957317f1e0315a6caea96bfc813d858254d4
Joran <joran.k@gmail.com>
2016-02-02 12:51:54 +1300

    add bower components to assets path so sprokets take them over

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,7 @@ module NzMakers
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Auckland'
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
